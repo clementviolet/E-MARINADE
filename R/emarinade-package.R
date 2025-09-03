dbb20086c3ae16ddfa9861b9c60e18865af9b2ae
Clement Violet <22980442+clementviolet@users.noreply.github.com>
2025-09-03 14:17:11 +0200

    Fixing package documentation level

--- R/emarinade-package.R
+++ R/emarinade-package.R
@@ -1,7 +1,12 @@
+## usethis namespace: start
+#' @importFrom dplyr filter
+## usethis namespace: end
+NULL
+
+
 #' emarinade: Explore Marine Non-Indigenous Species Data
 #'
 #' The **emarinade** package provides:
-#' 
 #' - Curated datasets: `taxo_tbl`, `inv_tbl`, `origin_tbl`, `meow`
 #' - A Shiny app for species search, taxonomy exploration, and mapping
 #' - Functions for working with invasion and native range data
@@ -12,12 +17,8 @@
 #' - `origin_tbl`: Native range records
 #' - `meow`: Marine ecoregions polygons
 #'
-#' @docType package
 #' @name emarinade
+#' @keywords internal
+#' @aliases emarinade-package emarinade
 #' @keywords package
 "_PACKAGE"
-
-## usethis namespace: start
-#' @importFrom dplyr filter
-## usethis namespace: end
-NULL
