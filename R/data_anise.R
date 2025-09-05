a8ce5168b5fd4c4f216589e9fdb69e56726c1a30
Clement Violet <22980442+clementviolet@users.noreply.github.com>
2025-09-05 11:43:28 +0200

    Add a function to load all the datasets at once

---
+++ R/data_anise.R
@@ -0,0 +1,17 @@
+#' Loading all anise datasets
+#'
+#' @param envir The environment where the datasets will be loaded. Defaults to the global environment.
+#'
+#' @returns Load the `taxo_tbl`, `inv_tbl`, `origin_tbl` and `meow` datasets.
+#' @seealso
+#' Dataset documentation: [taxo_tbl], [inv_tbl], [origin_tbl], [meow_eco].
+#' @examples
+#' \dontrun{
+#'   data_anise()
+#' }
+#' @export
+data_anise <- function(envir = .GlobalEnv) {
+  data(list = c("taxo_tbl", "inv_tbl", "origin_tbl", "meow"), 
+       package = "anise", 
+       envir = envir)
+}
